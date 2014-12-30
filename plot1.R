846ea0929f7854ae0dcc849d6bf89d7c1e6bbbb0
Roger Gong <roger.zgong@gmail.com>
2014-12-30 12:34:12 -0500

    update plot1.R and README.md

--- plot1.R
+++ plot1.R
@@ -1,6 +1,5 @@
-
 # Set the working directory to the directory where the data is saved
-setwd("/roger/Coursera/Exploratory_Analysis")
+setwd("/roger/Coursera/Exploratory_Analysis/ExData_Plotting1")
 
 # Read in all data from .txt file
 data <- read.table("household_power_consumption.txt", header = T, sep = ";", 
@@ -12,7 +11,9 @@ data$Date <- as.Date(data$Date, "%d/%m/%Y")
 # Subsetting the data between 02/01/2007 and 02/02/2007
 dataSubset <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
 
-# plot the histogram of Global Active Power
+# Plot1: Plot the histogram of Global Active Power
+png("plot1.png", height = 480, width = 480)
+par(mar = c(4.5, 4.5, 1.5, 1.5))
 hist(as.numeric(dataSubset$Global_active_power), main = "Global Active Power", 
      col = "Red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
-
+dev.off()
